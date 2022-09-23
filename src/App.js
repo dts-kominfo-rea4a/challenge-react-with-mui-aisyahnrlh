581c859079afc43a3ffc7a2571b7e05bdc9a6585
Aisyah Nurul Hidayah <aisyahnurulh13@gmail.com>
2022-09-23 07:56:35 +0700

    add logic for components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Contact from "./components/Contact";
+import ContactForm from "./components/ContactForm";
+import contactsJSON from "./data/contacts.json";
+import Header from "./components/Header";
+import List from "@mui/material/List";
+import Grid from "@mui/material/Grid";
 
-// Uncomment untuk memuat daftar kontak
-// import contactsJSON from './data/contacts.json';
 const App = () => {
-  // Masukkan Header dan lakukan map untuk Contact ke dalam div App
-  // untuk membuat daftar kontak bisa menggunakan MUI list
-  // https://mui.com/material-ui/react-list/#folder-list
+  const [contacts, setContacts] = useState(contactsJSON);
 
-  // Masukkan contacts yang sudah didapat dalam JSON sebagai initial state
-  // Buatlah handler untuk menambahkan kontak baru yang akan dikirim ke ContactForm
+  const addContact = (newContact) => {
+    setContacts((state) => [...state, newContact]);
+  };
 
   return (
     <div className="App">
+      <Header />
+      <Grid container spacing={2} justifyContent="center" alignItems="center">
+        <Grid item xs={12} md={6}>
+          <ContactForm addContact={addContact} />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <List
+            sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+          >
+            {contacts.map((contact) => (
+              <Contact data={contact} key={contact.phone} />
+            ))}
+          </List>
+        </Grid>
+      </Grid>
     </div>
   );
 };
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,15 +1,81 @@
-// Form dapat dibuat dengan TextField
-// https://mui.com/material-ui/react-text-field/#basic-textfield
-// dan Card
-// https://mui.com/material-ui/react-card/#basic-card
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import { useState } from "react";
 
-const ContactForm = () => {
-    // Form berisi name, phone, email, dan photo url
-    // Buatlah state newContact berupa objek sesuai dengan data yang ada
+const ContactForm = ({ addContact }) => {
+  const [newContact, setNewContact] = useState({
+    name: "",
+    phone: "",
+    email: "",
+    photo_url: "",
+  });
 
-    return (
-        <> </>
-    );
-}
+  const onSubmit = (e, newContact) => {
+    e.preventDefault();
+    addContact(newContact);
+  };
+
+  return (
+    <>
+      <Box
+        onSubmit={(e) => onSubmit(e, newContact)}
+        component="form"
+        sx={{
+          width: "100%",
+          maxWidth: 360,
+          bgcolor: "background.paper",
+          padding: "1rem",
+        }}
+      >
+        <Stack>
+          <TextField
+            id="filled-basic"
+            label="Name *"
+            variant="filled"
+            sx={{ marginBottom: "1rem" }}
+            onChange={(e) =>
+              setNewContact((state) => ({ ...state, name: e.target.value }))
+            }
+          />
+          <TextField
+            id="filled-basic"
+            label="Phone *"
+            variant="filled"
+            sx={{ marginBottom: "1rem" }}
+            onChange={(e) =>
+              setNewContact((state) => ({ ...state, phone: e.target.value }))
+            }
+          />
+          <TextField
+            id="filled-basic"
+            label="Email *"
+            variant="filled"
+            sx={{ marginBottom: "1rem" }}
+            onChange={(e) =>
+              setNewContact((state) => ({ ...state, email: e.target.value }))
+            }
+          />
+          <TextField
+            id="filled-basic"
+            label="Photo URL *"
+            variant="filled"
+            sx={{ marginBottom: "2rem" }}
+            onChange={(e) =>
+              setNewContact((state) => ({
+                ...state,
+                photo_url: e.target.value,
+              }))
+            }
+          />
+          <Button variant="text" color="info" type="submit">
+            Add new
+          </Button>
+        </Stack>
+      </Box>
+    </>
+  );
+};
 
 export default ContactForm;
